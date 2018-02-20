c6eb68ab2043c48770ee283fec33fef90c31a99e
ChrisGoetze <ctgoetze@gmail.com>
2018-02-20 13:13:12 -0500

    create client id and secret

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-	res.send({
-		bye: 'buddy'
-	});
-});
+
+// clientID 532143248303-2rvqqs3onhp33pp1df9bupi1a06tts02.apps.googleusercontent.com
+// clientSecret PP3TBL2R3RbVkkGWZ0m6AaC9
+passport.use(new GoogleStrategy());
+
 
 const PORT = process.env.PORT || 5000;
 
